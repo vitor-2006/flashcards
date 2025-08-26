56b8e458f39c4071001ec846f68ed7bf002deafc
vitor-2006 <vitorbalbinotdellatorre@gmail.com>
2025-08-26 19:32:23 -0300

    express

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 import express, {Request, Response} from 'express'
+const data = require('../array')
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+app.get('/baralho', (req, res) => {
+    return res.send(baralho)
+})
+
 	
 app.listen(port, () => {
     console.log("Api iniciada na porta: " + port);
